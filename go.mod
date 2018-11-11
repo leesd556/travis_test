efa1606f875f54c9551ae337b611df488415f8ae
KIMSJ <leesd556@gmail.com>
2018-11-11 16:28:15 +0900

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module travis_test
+
+require github.com/urfave/cli v1.20.0
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/urfave/cli"
+)
 
 func main(){
 	fmt.Printf("Travis Test2")
+	cli.NewApp()
 }
